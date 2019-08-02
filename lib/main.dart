78f5b5b8cdd8d9082830f5517e8572346bea1c91
Yuki Matsukura <matsubokkuri@gmail.com>
2019-08-02 15:27:57 +0900

    added auth package but does not work on HA-09

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,22 @@
+import 'package:flutter_webview_demo/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MaterialApp(home:WebViewExample()));
+void main() => runApp(MyApp());
 
-class WebViewExample extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _WebViewExampleState();
-}
-
-class _WebViewExampleState extends State<WebViewExample> {
-  WebViewController _controller;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('郵便番号検索'),
-          /*
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {
-                _controller.loadUrl('https://www.twitch.tv/');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.add_comment),
-              onPressed: () {
-                showDialog(context: context, builder: (context) {
-                  return AlertDialog(title: Text('webviewの上に表示'),);
-                });
-              },
-            ),
-          ],
-          */
-          leading: new IconButton(
-            icon: new Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => _controller.goBack(),
-          ),
+          title: Text("Auth"),
         ),
-        body: WebView(
-          initialUrl: 'https://postcode.teraren.com/',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController controller) {
-            _controller = controller;
-          },
-        ),
+        body: AuthScreen(),
+      ),
     );
   }
 }
-
